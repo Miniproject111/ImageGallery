766e4e2372fa21dd820df17f784415a4c0257a9e
Devang Pandya <47469089+Devang77@users.noreply.github.com>
2022-11-08 13:57:51 +0530

    Navigation Added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react"
 
 const Home: React.FC = () => {
   const [search, setSearch] = useState("random")
+  const [page, setPage] = useState(1)
 
   const handleChange = (ev: any) => {
     setSearch((prev) => (ev.detail.value ? ev.detail.value : prev))
+    setPage((prev) => (ev.detail.value ? 1 : prev))
   }
   const [data, setData] = useState({
     total: 0,
@@ -16,10 +18,10 @@ const Home: React.FC = () => {
 
   // `https://api.unsplash.com/photos/fIq0tET6llw?client_id=A7QlyQ2pxZszePhVk3gzYaeTGy6zy4e11wUNBO7hrJc`
   useEffect(() => {
-    fetch(`https://api.unsplash.com/search/photos?query=${search}&client_id=A7QlyQ2pxZszePhVk3gzYaeTGy6zy4e11wUNBO7hrJc`)
+    fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${search}&client_id=A7QlyQ2pxZszePhVk3gzYaeTGy6zy4e11wUNBO7hrJc`)
       .then((response) => response.json())
       .then((data) => setData(data))
-  }, [search])
+  }, [search.page])
 
   console.log(data)
 
@@ -27,7 +29,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Image Gallery </IonTitle>
+          <IonTitle>Image Gallery {": " + page + " / 10"} </IonTitle>
         </IonToolbar>
         <IonToolbar>
           <IonSearchbar showClearButton="focus" placeholder="Random" onIonChange={(ev) => handleChange(ev)}></IonSearchbar>
@@ -43,6 +45,27 @@ const Home: React.FC = () => {
           ))}
         </IonRow>
       </IonContent>
+      <IonToolbar>
+        <IonTitle>Navigation Buttons</IonTitle>
+        <IonButtons slot="end">
+          <IonButton
+            disabled={page === 1}
+            onClick={() => {
+              setPage((prev) => prev - 1)
+            }}
+          >
+            <IonIcon slot="icon-only" icon={arrowBackOutline} />
+          </IonButton>
+          <IonButton
+            disabled={page === data.total}
+            onClick={() => {
+              setPage((prev) => prev + 1)
+            }}
+          >
+            <IonIcon slot="icon-only" icon={arrowForwardOutline} />
+          </IonButton>
+        </IonButtons>
+      </IonToolbar> 
     </IonPage>
   )
 }
